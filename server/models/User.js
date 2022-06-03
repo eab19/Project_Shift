c4cbaa432410190a85f42d25f3fe8116344dc78a
eab19 <boileau14@gmail.com>
2022-06-03 15:32:51 -0400

    server boilerplate added

---
+++ server/models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+const Product = require('./Product')
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  orders: [Order.schema],
+  products: [Product.schema]
+},
+{
+  toJSON: {
+    virtuals: true,
+  }
+}
+);
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,81 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type Category {
+    _id: ID
+    name: String
+  }
+
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    category: Category
+    user: String
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    orders: [Order]
+    products: [Product]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  input productData {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    quantity: Int
+    price: Float
+    user: String
+  }
+
+  input categoryData {
+    name: String
+  }
+
+  type Query {
+    categories: [Category]
+    users: [User]
+    me: User
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    productFormAdd(name: String, description: String, image: String, quantity: Int, price: Float, user: String): Product
+    updateProduct(_id: ID!, quantity: Int!): Product
+    editProduct(productId: ID!, productData: productData): Product
+    login(email: String!, password: String!): Auth
+    addProduct(userId: ID!, productData: productData): Product
+    removeProduct(productId: ID!): Product
+  }
+`;
+
+module.exports = typeDefs;
